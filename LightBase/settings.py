2112bd0be77f4329a437649148aabc124fd5e89f
rabbani <nawras.rabbani@mail.mcgill.ca>
2017-10-25 23:30:15 -0400

    set up envrionment for front end developpement of index

--- LightBase/settings.py
+++ LightBase/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'recommender',
 ]
 
 MIDDLEWARE = [
@@ -115,6 +116,12 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.11/howto/static-files/
+# https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
+
+MEDIA_ROOT = './media'
+
+MEDIA_URL = '/media/'
--- recommender/views.py
+++ recommender/views.py
@@ -6,4 +6,4 @@ from django.shortcuts import render,HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    return render(request, 'recommender/index.html')
